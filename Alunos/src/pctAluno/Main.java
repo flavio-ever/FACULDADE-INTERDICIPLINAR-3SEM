1a6c6939fcd4c0c8374e61b2176b903af4f23693
Flavio Web <onew_@outlook.com>
2018-10-25 23:46:29 -0300

    Add files via upload

---
+++ Alunos/src/pctAluno/Main.java
@@ -0,0 +1,39 @@
+package pctAluno;
+import java.util.*;
+
+class Main {
+  public static void main(String[] args) {
+    Aluno teste = new Aluno();
+
+    List<Aluno> objAluno = new ArrayList<Aluno>();
+
+    List<Integer>   notasAluno = new ArrayList<Integer>();
+                    notasAluno.add(10);
+                    notasAluno.add(9);
+                    notasAluno.add(10);
+
+     objAluno.add(new Aluno("1863036-7", 
+                            "Flavio Everton", 
+                            22, 
+                            "ADS", 
+                            notasAluno));
+
+     objAluno.add(new Aluno("6666666-7", 
+                            "Kalita Trevisan", 
+                            23, 
+                            "XS", 
+                            notasAluno));
+
+    //  Aluno x = teste.SearchAluno(objAluno, "1863036-7", "");
+    //  System.out.println(x.getNomeAluno());
+    // x.setNomeAluno("Puta");
+
+    // Aluno z = teste.SearchAluno(objAluno, "1863036-7", "");
+    // 			z.editAluno();
+    // 			System.out.println(x.getNomeAluno());
+    // 			System.out.println(z.getNomeAluno());
+
+    teste.SearchAluno(objAluno, null, 22);
+  }
+}
+
